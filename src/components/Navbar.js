5d2d2574e9a1f9d3d3350c57412602e2574d1dda
Ahsan Zafar <engrahsanrazaa@gmail.com>
2024-10-24 11:24:10 +0500

    # added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} `} data-bs-theme={props.mode}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarScroll">
               <ul className={`navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll text-${props.mode === 'light' ? 'dark' : 'light'}`}>
               <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                  <a className="nav-link" href="/">{props.aboutText}</a>
+                  <a className="nav-link" href="#">{props.aboutText}</a>
               </li>
               </ul>
               <div className="d-flex">
